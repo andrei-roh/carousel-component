48303a857e15b597d2c845805d703dfdc20b4cc5
Andrei Roh <andrei_roh@tut.by>
2021-05-27 08:18:39 +0300

    feat: add mouse movements
    Update code

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -15,26 +15,71 @@ const Carousel = () => {
     <img key={slide5} src={slide5} />,
   ];
   const [activeIndex, setActiveIndex] = useState(0);
-  const activeIndexDown = () => {
-    activeIndex === 0
-    ? setActiveIndex(activeIndex + 4)
-    : setActiveIndex(activeIndex - 1);
+  const prevImage = () => {
+    setActiveIndex(current => {
+      return (current === 0) ? current = (img.length - 1) : current - 1
+    });
   };
-  const activeIndexUp = () => {
-    activeIndex === (img.length - 1)
-    ? setActiveIndex(activeIndex - 4)
-    : setActiveIndex(activeIndex + 1);
+  const nextImage = () => {
+    setActiveIndex(current => {
+      return (current === img.length - 1) ? 0 : current + 1
+    });
   };
+  const prevIndex = activeIndex === 0
+    ? activeIndex + (img.length - 1)
+    : activeIndex - 1;
+  const nextIndex = activeIndex === (img.length - 1)
+    ? activeIndex - (img.length - 1)
+    : activeIndex + 1;
 
+  let isDown = false;
+  let startOnX;
+  let offset = 0;
   return (
     <div className="sliderBlock">
-      <button className="sliderButton" onClick={activeIndexDown}>Previous</button>
-      <div className="slider">
-        <div className="slider-img" key={activeIndex}>
-          {img[activeIndex]}
+      <div
+        className="sliderScreen"
+        onMouseDown={(element) => {
+          const sliderScreen = document.querySelector('.sliderScreen');
+          isDown = true;
+          startOnX = element.pageX - sliderScreen.offsetLeft;
+        }}
+        onMouseLeave={() => isDown = false}
+        onMouseMove={(element) => {
+          const sliderScreen = document.querySelector('.sliderScreen');
+          if(!isDown) return;
+          element.preventDefault();
+          const x = element.pageX - sliderScreen.offsetLeft;
+          offset = (x - startOnX) * 2;
+          return offset
+        }}
+        onMouseUp={() => {
+          isDown = false;
+          if(offset >= 120) {
+            prevImage();
+          }
+          if(offset <= -120) {
+            nextImage();
+          }
+          offset = 0;
+        }}
+      >
+        <div className="sliderImage sliderImagePrevious" key={prevIndex}>
+          {img[prevIndex]}
+        </div>
+        <div className="sliderImage" key={activeIndex}>
+          <button className="sliderButton sliderButtonLeft" onClick={prevImage}>
+            <div className="previousIcon" />
+          </button>
+          <div>{img[activeIndex]}</div>
+          <button className="sliderButton sliderButtonRight" onClick={nextImage}>
+            <div className="nextIcon" />
+          </button>
+        </div>
+        <div className="sliderImage sliderImageNext" key={nextIndex}>
+          {img[nextIndex]}
         </div>
       </div>
-      <button className="sliderButton" onClick={activeIndexUp}>Next</button>
     </div>
   )
 }
