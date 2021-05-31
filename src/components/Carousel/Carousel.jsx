9f7be93d7a6b3514c19469547562d2ab4eb00870
Andrei Roh <andrei_roh@tut.by>
2021-05-31 17:31:23 +0300

    feat: update app
    Change app logic. Add mouse touch, swipes, refactor code, update function

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,93 +1,151 @@
 import React, { useState } from 'react';
+import slides from './defaultSlides';
 import './style.css';
 
 const Carousel = () => {
-  const slide1 = 'images/bear.jpg';
-  const slide2 = 'images/bird.jpg';
-  const slide3 = 'images/cat.jpg';
-  const slide4 = 'images/dog.jpg';
-  const slide5 = 'images/manul.jpg';
-  const images = [
-    <img key={slide1} src={slide1} />,
-    <img key={slide2} src={slide2} />,
-    <img key={slide3} src={slide3} />,
-    <img key={slide4} src={slide4} />,
-    <img key={slide5} src={slide5} />,
-  ];
-  const [activeIndex, setActiveIndex] = useState(0);
-  const prevImage = () => {
-    setActiveIndex(current => {
-      return (current === 0) ? current = (images.length - 1) : current - 1
+  const slideLength = 390;
+  const slideHeight = 260;
+  const carouselEdge = Math.floor(slides.length / 2);
+  const slidesPositions = [(slideLength * 2), slideLength, 0, -slideLength, (-slideLength * 2)];
+  const [activePosition, setActivePosition] = useState(0);
+  const [activeDirection, setActiveDirection] = useState(0);
+  const prevSlide = () => {
+    setActivePosition(current => {
+      return (
+        (current / slideLength) === carouselEdge)
+          ? current = (-slideLength * carouselEdge)
+          : current + slideLength;
     });
   };
-  const nextImage = () => {
-    setActiveIndex(current => {
-      return (current === images.length - 1) ? 0 : current + 1
+  const nextSlide = () => {
+    setActivePosition(current => {
+      return (
+        (current / slideLength) === (-carouselEdge))
+          ? current = (slideLength * carouselEdge)
+          : current - slideLength;
     });
-  };
-  const prevIndex = activeIndex === 0
-    ? activeIndex + (images.length - 1)
-    : activeIndex - 1;
-  const nextIndex = activeIndex === (images.length - 1)
-    ? activeIndex - (images.length - 1)
-    : activeIndex + 1;
-  const getActiveIndex = (element) => {
-    setActiveIndex(element)
   };
 
   let isDown = false;
   let startOnX;
   let offset = 0;
+  let scrollLeft;
+
+  const setOnUpChangeSlide = () => {
+    isDown = false;
+    if (activePosition > slideLength) {
+      if (activeDirection > 0) {
+        return setActivePosition(slideLength * 2);
+      };
+      if (activeDirection < 0) {
+        return setActivePosition(slideLength);
+      };
+    };
+    if (activePosition >= 60 && activePosition <= slideLength) {
+      if (activeDirection > 0) {
+        return setActivePosition(slideLength);
+      };
+      if (activeDirection < 0) {
+        return setActivePosition(0);
+      };
+    };
+    if (activePosition <= -60 && activePosition >= -slideLength) {
+      if (activeDirection < 0) {
+        return setActivePosition(-slideLength);
+      };
+      if (activeDirection > 0) {
+        return setActivePosition(0);
+      };
+    };
+    if (activePosition < -slideLength) {
+      if (activeDirection < 0) {
+        return setActivePosition(-slideLength * 2);
+      };
+      if (activeDirection > 0) {
+        return setActivePosition(-slideLength);
+      };
+    };
+    setActivePosition(0)
+  }
+
   return (
     <div className="sliderBlock">
       <div
         className="sliderScreen"
-        onMouseDown={(element) => {
-          const sliderScreen = document.querySelector('.sliderScreen');
-          isDown = true;
-          startOnX = element.pageX - sliderScreen.offsetLeft;
-        }}
-        onMouseLeave={() => isDown = false}
-        onMouseMove={(element) => {
-          const sliderScreen = document.querySelector('.sliderScreen');
-          if(!isDown) return;
-          element.preventDefault();
-          const x = element.pageX - sliderScreen.offsetLeft;
-          offset = (x - startOnX) * 2;
-          return offset
-        }}
-        onMouseUp={() => {
-          isDown = false;
-          if(offset >= 120) {
-            prevImage();
-          }
-          if(offset <= -120) {
-            nextImage();
-          }
-          offset = 0;
-        }}
+        style={{ width: `${slideLength}px`}}
       >
-        <div className="sliderImage sliderImagePrevious" key={prevIndex}>
-          {images[prevIndex]}
-        </div>
-        <div className="sliderImage" key={activeIndex}>
-          <button className="sliderButton sliderButtonLeft" onClick={prevImage}>
-            <div className="previousIcon" />
-          </button>
-          <div>{images[activeIndex]}</div>
-          <button className="sliderButton sliderButtonRight" onClick={nextImage}>
-            <div className="nextIcon" />
-          </button>
-          <div className="buttonsKit">
-            <button className="buttonInKit" onClick={() => getActiveIndex(0)} />
-            <button className="buttonInKit" onClick={() => getActiveIndex(1)} />
-            <button className="buttonInKit" onClick={() => getActiveIndex(2)} />
-            <button className="buttonInKit" onClick={() => getActiveIndex(3)} />
-            <button className="buttonInKit" onClick={() => getActiveIndex(4)} />
-          </div>
-        </div>
-        <div className="sliderImage sliderImageNext" key={nextIndex}>
-          {images[nextIndex]}
+        <button
+          className="sliderButton sliderButtonLeft"
+          style={{ minHeight: `${slideHeight}px`}}
+          onClick={prevSlide}
+        >
+          <div className="previousIcon" />
+        </button>
+        {slides.map(
+          slide =>
+            <img
+              className="sliderSlide"
+              style={{ left: `${activePosition}px`, height: `${slideHeight}px` }}
+              key={slide.key}
+              src={slide.src}
+              onMouseDown={(element) => {
+                element.preventDefault();
+                const sliderScreen = document.querySelector('.sliderSlide');
+                isDown = true;
+                startOnX = element.pageX - sliderScreen.offsetLeft;
+                scrollLeft = sliderScreen.scrollLeft;
+              }}
+              onMouseLeave={
+                () => {
+                  isDown = false
+                }
+              }
+              onMouseMove={(element) => {
+                const sliderScreen = document.querySelector('.sliderSlide');
+                if(!isDown) return;
+                element.preventDefault();
+                const x = element.pageX - sliderScreen.offsetLeft;
+                offset = (x - startOnX) * 2;
+                sliderScreen.scrollLeft = scrollLeft - offset;
+                offset > 0 ? setActiveDirection(1) : setActiveDirection(-1);
+                setActivePosition(activePosition + (offset * 30))
+              }}
+              onMouseUp={setOnUpChangeSlide}
+              onTouchStart={(element) => {
+                const sliderScreen = document.querySelector('.sliderSlide');
+                isDown = true;
+                startOnX = element.touches[0].pageX - sliderScreen.offsetLeft;
+                console.log(startOnX)
+                scrollLeft = sliderScreen.changedTouches;
+              }}
+              onTouchMove={(element) => {
+                const sliderScreen = document.querySelector('.sliderSlide');
+                if(!isDown) return;
+                const x = element.touches[0].pageX - sliderScreen.offsetLeft;
+                offset = (x - startOnX);
+                sliderScreen.changedTouches = scrollLeft - offset;
+                offset > 0 ? setActiveDirection(1) : setActiveDirection(-1);
+                setActivePosition(activePosition + (offset * 30))
+              }}
+              onTouchEnd={setOnUpChangeSlide}
+            />
+        )}
+        <button
+          className="sliderButton sliderButtonRight"
+          style={{ minHeight: `${slideHeight}px`}}
+          onClick={nextSlide}
+        >
+          <div className="nextIcon" />
+        </button>
+        <div className="buttonsKit">
+          {slides.map(
+            slide =>
+              <button
+                key={slide.key}
+                className="buttonInKit"
+                onClick={() => setActivePosition(slidesPositions[slides.indexOf(slide)])}
+              />
+          )}
         </div>
       </div>
     </div>
---
+++ src/components/Carousel/defaultSlides.js
@@ -0,0 +1,15 @@
+const slide1 = 'images/bear.jpg';
+const slide2 = 'images/bird.jpg';
+const slide3 = 'images/cat.jpg';
+const slide4 = 'images/dog.jpg';
+const slide5 = 'images/manul.jpg';
+
+const slides = [
+  {key: slide1, src: slide1},
+  {key: slide2, src: slide2},
+  {key: slide3, src: slide3},
+  {key: slide4, src: slide4},
+  {key: slide5, src: slide5},
+];
+
+export default slides;
